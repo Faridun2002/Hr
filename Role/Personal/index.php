f5b20e0916ce6233d9eb848c31f7d364725a5679
noolfj <firdavsjuraev183@gmail.com>
2024-05-06 08:10:18 +0500

    Исправил фотографию по умолчанию

--- Role/Personal/index.php
+++ Role/Personal/index.php
@@ -1,6 +1,29 @@
 <?php 
 require_once "header.php";
+
+session_start();
+if( empty($_SESSION["Username"]) ){
+    header("Location: ./login.php");
+}
+
+require_once "conn.php";
+
+$sql_command = "SELECT e.*, d.Degree_Name, e.Path_Photo 
+                FROM employees e 
+                INNER JOIN degrees d ON e.Degree_Id = d.Degree_Id 
+                WHERE e.Username = '{$_SESSION['Username']}'";
+
+$result = mysqli_query($conn, $sql_command);
+
+if (mysqli_num_rows($result) > 0) {
+    while ($rows = mysqli_fetch_assoc($result)) {
+        $Full_Name = ucwords($rows["Full_Name"]);
+        $Degree_Name = $rows["Degree_Name"];    
+        $Path_Photo = $rows["Path_Photo"];   
+    }
+}
 ?>
+
 <div class="main-content">
 
     <div class="page-content">
@@ -41,15 +64,15 @@ require_once "header.php";
                                                 <td class="align-middle">
                                                     <div class="d-flex align-items-center">
                                                         <div class="me-4">
-                                                            <img src="../assets/images/users/avatar-7.jpg" alt=""
+                                                            <img src="upload/<?php if(!empty($Path_Photo)){ echo $Path_Photo; }else{ echo "1.jpg"; } ?>" alt=""
                                                                 class="avatar-sm rounded-circle">
                                                         </div>
                                                         <div>
-                                                            <h5 class="font-size-16 mb-1">Ахмедов Анвар</h5>
+                                                            <h5 class="font-size-16 mb-1"><?php echo $Full_Name; ?></h5>
                                                         </div>
                                                     </div>
                                                 </td>
-                                                <td class="align-middle">Доктор Наук</td>
+                                                <td class="align-middle"><?php echo $Degree_Name; ?></td>
                                                 <td class="align-middle">20/40</td>
                                                 <td class="align-middle">33</td>
                                             </tr>
